57cd8ebb9e4c5b0b491151866976e6574f57ec2d
Felix Reichel <f.reichel@gmx.at>
2021-11-30 23:14:01 +0100

    Update 22_AR-1_parameter_estimation.R

--- 551OKMEZRAK14_Time_Series_Analysis_Statistics_JKU/22_AR-1_parameter_estimation.R
+++ 551OKMEZRAK14_Time_Series_Analysis_Statistics_JKU/22_AR-1_parameter_estimation.R
@@ -8,42 +8,63 @@ require(tseries)
 source("https://raw.githubusercontent.com/robjhyndman/forecast/master/R/armaroots.R")
 
 Tau <- 100000
+Tau_subset <- c(50, 100, 1000, 10000)
 sgma2 <- 1
 phi <- .6
-mu <- 10 # where is the unconditional mean
-
-Tau_subset <- c(50, 100, 1000, 10000)
+mu <- 10 # unconditional mean
 
 set.seed(2345)
 
-ARIMA_order <- c(1,0,0)
-ts.sim <- mu + arima.sim(list(order = ARIMA_order, ar = c(phi)), n = Tau, sd = sgma2)
+AR_1 <- c(1,0,0)
+AR_1.sim <- mu + arima.sim(list(order = AR_1, ar = c(phi)), n = Tau, sd = sgma2)
+plot(AR_1.sim[1:100], type = "l")
 
-plot(ts.sim[1:100], type = "l")
+ar(AR_1.sim[1:Tau_subset[1]], method = "yule-walker")
+plot(ar(AR_1.sim[1:Tau_subset[1]], method = "yule-walker"))
+ar(AR_1.sim[1:Tau_subset[1]], method = "ols")
+plot(ar(AR_1.sim[1:Tau_subset[1]], method = "ols"))
+ar(AR_1.sim[1:Tau_subset[1]], method = "mle")
+plot(ar(AR_1.sim[1:Tau_subset[1]], method = "mle"))
 
+# No for-loop just for pdf knitting:
+ar(ts.sim[1:Tau_subset[2]], method = "yule-walker")
+ar(ts.sim[1:Tau_subset[2]], method = "ols")
+ar(ts.sim[1:Tau_subset[2]], method = "mle")
 
-# Estimate the parameters using Yule-Walker equations, OLS, MLE
-ar(ts.sim[1:Tau_subset[1]], method = "yule-walker")
-plot(ar(ts.sim[1:Tau_subset[1]], method = "yule-walker"))
-
-ar(ts.sim[1:Tau_subset[1]], method = "ols")
-plot(ar(ts.sim[1:Tau_subset[1]], method = "ols"))
-
-ar(ts.sim[1:Tau_subset[1]], method = "mle")
-plot(ar(ts.sim[1:Tau_subset[1]], method = "mle"))
-
-
-for (idx in 2:4) {
-  ar(ts.sim[1:Tau_subset[idx]], method = "yule-walker")
-  ar(ts.sim[1:Tau_subset[idx]], method = "ols")
-  ar(ts.sim[1:Tau_subset[idx]], method = "mle")
-}
+ar(ts.sim[1:Tau_subset[3]], method = "yule-walker")
+ar(ts.sim[1:Tau_subset[3]], method = "ols")
+ar(ts.sim[1:Tau_subset[3]], method = "mle")
 
 
-x1 <- 2:50 # yt to yT*1
+ar(AR_1.sim[1:Tau_subset[4]], method = "yule-walker")
+plot(ar(AR_1.sim[1:Tau_subset[4]], method = "yule-walker"))
+ar(AR_1.sim[1:Tau_subset[4]], method = "ols")
+plot(ar(AR_1.sim[1:Tau_subset[4]], method = "ols"))
+ar(AR_1.sim[1:Tau_subset[4]], method = "mle")
+plot(ar(AR_1.sim[1:Tau_subset[4]], method = "mle"))
 
-ts.sim.demeaned = ts.sim[x1] - mu
-plot(ts.sim.demeaned[x1], type ="l")
 
-lm = lm(x1 ~ ts.sim.demeaned[x1])
-lm
+# No for-loop just for pdf knitting:
+t <- 2:Tau_subset[1] # yt to yT*1
+AR_1.sim.demeaned = AR_1.sim[t] - mu
+lm = lm(AR_1.sim.demeaned[t] ~ t)
+summary(lm)
+lm$coefficients
+
+t <- 2:Tau_subset[2] # yt to yT*1
+AR_1.sim.demeaned = AR_1.sim[t] - mu
+lm = lm(AR_1.sim.demeaned[t] ~ t)
+summary(lm)
+lm$coefficients
+
+t <- 2:Tau_subset[3] # yt to yT*1
+AR_1.sim.demeaned = AR_1.sim[t] - mu
+lm = lm(AR_1.sim.demeaned[t] ~ t)
+summary(lm)
+lm$coefficients
+
+t <- 2:Tau_subset[4] # yt to yT*1
+AR_1.sim.demeaned = AR_1.sim[t] - mu
+lm = lm(AR_1.sim.demeaned[t] ~ t)
+summary(lm)
+lm$coefficients
